b3f115deb1272435b29070fe471bb8f1e6ddfff7
joaopedroalc <joaopedro.ufma@gmail.com>
2022-12-06 17:35:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+//fonte https://www.emqx.com/en/blog/how-to-use-mqtt-in-nodejs
+
+console.log("Hello world!");
+
+const mqtt = require('mqtt')
+
+const host = 'broker.emqx.io'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${host}:${port}`
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
+
+const pubTopic = 'TESTE/output'
+const subTopic = 'TESTE/input'
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([subTopic], () => {
+    console.log(`Subscribe to topic '${subTopic}'`)
+
+  })
+  client.publish(pubTopic, 'Publicando do NodeJS!!', { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+})
+
+
+client.on('message', (subTopic, payload) => {
+  console.log('Received Message:', subTopic, payload.toString())
+})
+
